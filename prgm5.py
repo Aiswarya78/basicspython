26969fee578d87e13d244aa1aad3f18e206b9907
aiswarya s <aiswarya.s4528@gmail.com>
2023-09-28 15:04:03 +0530

    done few  basic programs

---
+++ prgm5.py
@@ -0,0 +1,7 @@
+num1=int(input("enter a number"))
+num2=int(input("enter another number"))
+print("the numbers before swap are",num1,"and",num2)
+temp=num1
+num1=num2
+num2=temp
+print("the number after swap are",num1,"and",num2)
